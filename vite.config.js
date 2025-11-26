13b621a51aa91765c48c0e1125b78f29f72558b0
osricgjacobs <osricgjacobs@gmail.com>
2025-11-26 14:49:56 +0200

    vite config updated3

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const repoName = 'resume'; 
-
+// https://vite.dev/config/
+const repoName = "resume";
 export default defineConfig({
-  // CRITICAL: This prepends the repo name to all asset links in index.html
-  base: `/${repoName}/`, 
-  
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  base: `/${repoName}/`,
 });
